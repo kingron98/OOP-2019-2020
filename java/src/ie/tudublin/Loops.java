a30b71d1ac11020f187e1463b197b05375b51b79
unknown <aaronfu123@gmail.comgit config --global user.name AaronOmit --$>
2020-02-11 22:46:20 +0000

    lab3

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -9,20 +9,53 @@ public class Loops extends PApplet
 		size(500, 500);
 	}
 
+	float x, y, xx, yy;
+	int count = 0;
+
 	public void setup() 
 	{
-		
+		colorMode(HSB);
+		x = 250;
+		y = 250;
+		xx = 0;
+		yy = 0;
 	}
 
 	
 	public void keyPressed()
 	{
+		stroke(0);
 
+		// moire pattern
+		// strokeWeight(3);
+		// for(int i = 0; i <= width; i += 25)
+		// {
+		// 	line(x, y, i, yy);
+		// }
+		// for(int i = 0; i <= width; i += 25)
+		// {
+		// 	line(x, y, xx, i);
+		// }
+		// for(int i = 0; i <= width; i += 25)
+		// {
+		// 	line(x, y, i, width);
+		// }
+		// for(int i = 0; i <= width; i += 25)
+		// {
+		// 	line(x, y, width, i);
+		// }
+		
+		// circle loop pattern
+		// for(x = 50; x < 500; x+=50)
+		// {
+		// 	ellipse(x, 50, 50, 50);
+		// }
 	}	
 
 
 	public void draw()
 	{	
-		background(0);		
+		background(200);		
+		keyPressed();
 	}
 }
